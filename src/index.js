894cc44e5fa5bc4397fb9a957a2c6df7c8807932
AlexAbramoff <56651177+AlexAbramoff@users.noreply.github.com>
2021-03-07 18:36:36 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arrStr = str.split("");
+    let stack = [];
+    let subArr = [];
+    for (i = 0; i < arrStr.length; i++) {
+        subArr[0] = stack[stack.length - 1];
+        subArr[1] = arrStr[i];
+        let k = false;
+        for (j = 0; j < bracketsConfig.length; j++) {
+            if (bracketsConfig[j].join("") == subArr.join("")) {
+                stack.pop();
+                k = true;
+            }
+        }
+        if (!k) {
+            stack.push(arrStr[i]);
+        }
+    }
+    return stack.length == 0;
+};
